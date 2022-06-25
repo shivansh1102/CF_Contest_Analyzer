b89d5e6e43b260d2e48b37bb5fb4bb3b3e0c5a58
Shivansh Shukla <shivanshshukla60@gmail.com>
2022-06-26 03:08:16 +0530

    TimeAnalysis Done & Tested

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const path = require("path")
 const bodyParser = require("body-parser");
 const app = express();
 const https = require("https");
-const ejsmate = require("ejs-mate")
+const ejsmate = require("ejs-mate");
+const { start } = require("repl");
 
 app.engine('ejs', ejsmate)
 app.set('views', path.join(__dirname, 'views'))
@@ -18,23 +19,30 @@ app.get("/", (req, res) => {
     res.render("analysis",{display:0});
 });
 
-class timeProblem
+class timeProblem // this is used in storing fastest & slowest submission {time, contestID} so that link can be provided to user
 {
     contructor(submitTime = 0, contestID = "") {
         this.submitTime = submitTime;
         this.contestID = contestID;
     }
 }
+class RatingProblem{ // this is used in storing max rated problem {rating, contestID}
+    contructor(rating = 0, contestID = "") {
+        this.rating = rating;
+        this.contestID = contestID;
+    }
+}
 class ProblemAnalysis {
-    constructor(cntSubmission = 0, cntAC = 0, sumSubmitTime = 0, fastestSubmission = new timeProblem(), slowestSubmission = new timeProblem(), maxRatedProblem = 0) {
+    constructor(cntSubmission = 0, cntAC = 0, sumSubmitTime = 0, fastestSubmission = new timeProblem(), slowestSubmission = new timeProblem(), maxRatedProblem = new RatingProblem()) {
         this.cntSubmission = cntSubmission;
         this.cntAC = cntAC;
         this.sumSubmitTime = sumSubmitTime;
         this.fastestSubmission = fastestSubmission;
-        this.fastestSubmission.submitTime = 0;
+        this.fastestSubmission.submitTime = Infinity;
         this.slowestSubmission = slowestSubmission;
-        this.slowestSubmission.submitTime = Infinity;
+        this.slowestSubmission.submitTime = 0;
         this.maxRatedProblem = maxRatedProblem;
+        this.maxRatedProblem.rating = 0;
     }
     countWA() {
         return this.cntSubmission - this.cntAC;
@@ -49,38 +57,71 @@ class ProblemAnalysis {
         return (this.sumSubmitTime / this.cntAC) * 100;
     }
 }
-
 let problems = [];
-for(let i = 0; i < 10; i++)
-problems[i] = new ProblemAnalysis();
+function resetArray()
+{
+    problems = [];
+    for(let i = 0; i < 10; i++)
+    {
+        problems[i] = new ProblemAnalysis();
+    }
+}
 
-const problemIndex = {
-    "A" : 0,
-    "B" : 1,
-    "C" : 2,
-    "D" : 3,
-    "E" : 4,
-    "F" : 5
-};
+
+
 function findIndex(index)
 {
-    return problemIndex.index;
+    let ans;
+    switch(index)
+    {
+        case "A" :
+            ans = 0;
+            break;
+        case "B" :
+            ans = 1;
+            break;
+        case "C" :
+            ans = 2;
+            break;
+        case "D" :
+            ans = 3;
+            break;
+        case "E" :
+            ans = 4;
+            break;    
+        case "F" : 
+            ans = 5;
+            break;
+    }
+    return ans;
 }
-function timeAnalysis(result, lowerTime, upperTime)
+
+function timeAnalysis(result, lowerTime, upperTime, problems)
 {
-    result.forEach(submission => {
+    result.forEach((submission)=>{
         if(submission.creationTimeSeconds >= lowerTime && submission.creationTimeSeconds <= upperTime && submission.author.participantType === "CONTESTANT")
         {
-            const index = findIndex(submission.problem.index);
-            problems[i].cntSubmission++;
+            const index = submission.problem.index.substring(0,1);
+            const i = findIndex(index);
+            problems[i].cntSubmission += 1;
             if(submission.verdict === "OK")
             {
                 problems[i].cntAC++;
                 problems[i].sumSubmitTime += submission.relativeTimeSeconds;
-                if(submission.relativeTimeSeconds > problems[i].fastestSubmission.submitTime)
+                if(submission.relativeTimeSeconds < problems[i].fastestSubmission.submitTime)
                 {
                     problems[i].fastestSubmission.submitTime = submission.relativeTimeSeconds;
-                    problems[i].fastestSubmission.contestID = submission.author.contestId
+                    problems[i].fastestSubmission.contestID = submission.author.contestId;
+                }
+                if(submission.relativeTimeSeconds > problems[i].slowestSubmission.submitTime)
+                {
+                    problems[i].slowestSubmission.submitTime = submission.relativeTimeSeconds;
+                    problems[i].slowestSubmission.contestID = submission.author.contestId;
+                }
+                if(submission.problem.rating > problems[i].maxRatedProblem.rating)
+                {
+                    problems[i].maxRatedProblem.rating = submission.problem.rating;
+                    problems[i].maxRatedProblem.contestID = submission.author.contestId;
                 }
             }
         }
@@ -99,8 +140,11 @@ const generalAnalysis=(result,ratingResult)=>{
 }
 
 app.post("/", (req, res) => {
-    const {handle,startDate,endDate} = req.body;
-    const url = "https://codeforces.com/api/user.status?handle=" + handle;
+    resetArray();
+    let {handle,startDate,endDate} = req.body;
+    startDate = parseInt((new Date(startDate).getTime() / 1000).toFixed(0));
+    endDate = parseInt((new Date(endDate).getTime() / 1000).toFixed(0));
+    const url = "https://codeforces.com/api/user.status?handle=" + handle ;
     const ratingUrl = "https://codeforces.com/api/user.rating?handle="+ handle;
     console.log(url);
     https.get(url, (response) => {
@@ -111,8 +155,9 @@ app.post("/", (req, res) => {
             chunks.push(data);
         }).on("end", () => {
             let data = Buffer.concat(chunks);
-            let result = JSON.parse(data);
-            // console.log(result);
+            let result = JSON.parse(data).result;
+            timeAnalysis(result, startDate, endDate, problems);
+            console.log(problems);
         })
     })
 
